1a92d8f69a3c4186af1b9294b5fc8e9fac215a6a
ebriussenex <ebriussenex@gmail.com>
2023-12-26 23:55:05 +0300

    feat: response to connection

--- app/server.go
+++ app/server.go
@@ -6,6 +6,15 @@ import (
 	"os"
 )
 
+const (
+	crlf = "\r\n"
+)
+
+type StatusCode struct {
+	text string
+	code uint8
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -14,10 +23,27 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("accepted connection")
+
+		var result []byte
+
+		if _, err := connection.Read(result); err != nil {
+			fmt.Println("Failed to read from connection")
+		}
+
+		statusLine := formStatusLine("HTTP v1.1", StatusCode{"OK", 200})
+		connection.Write([]byte(statusLine))
 	}
+}
+
+func formStatusLine(protocolVersion string, statusCode StatusCode) string {
+	return fmt.Sprintf("%s %d %s", protocolVersion, statusCode.code, statusCode.text) + crlf + crlf
 }
